838c4971d5afb8ed6ced05b952a2289e447aff64
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-03 14:09:55 +0930

    Modified printf_un_int.c

--- cfiles/printf_un_int.c
+++ cfiles/printf_un_int.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 15:34:41 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/03 14:04:02 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/05/03 14:08:42 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,19 @@ static size_t	ft_un_int_len(unsigned int nb)
 
 static char	*ft_unitoa(unsigned int n)
 {
+	char	*nb;
+	size_t	len;
+
+	len = ft_un_int_len(n);
+	nb = (char *)malloc(sizeof(char) * (len + 1));
+	if (!nb)
+		return (NULL);
+	nb[len] = '\0';
+	while (n != 0)
+	{
+		nb[len - 1] = n % 10 + 48;
+		n /= 10;
+		len--;
+	}
+	return (nb);
 }
