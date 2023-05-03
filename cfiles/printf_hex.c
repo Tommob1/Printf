3c9681ac091e5a050196803f6889c37b3a470063
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-03 16:54:20 +0930

    Modified printf_hex.c file

--- cfiles/printf_hex.c
+++ cfiles/printf_hex.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:48:05 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/03 14:49:44 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:52:27 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,43 @@
 
 static int	ft_hexlen(unsigned int nb)
 {
+	int	len;
+
+	len = 0;
+	while (nb != 0)
+	{
+		len++;
+		nb = nb / 16;
+	}
+	return (len);
 }
 
 static void	ft_pthex(unsigned int nb, const char format)
 {
+	if (nb >= 16)
+	{
+		ft_pthex(nb /16, format);
+		ft_pthex(nb % 16, format);
+	}
+	else
+	{
+		if (nb <= 9)
+			printf_char(nb + 48);
+		else
+		{
+			if (format == 'x')
+				printf_char(nb - 10 + 'a');
+			if (format == 'X')
+				printf_char(nb - 10 + 'A');
+		}
+	}
 }
 
 int	printf_pthex(unsigned int nb, const char format)
 {
+	if (nb == 0)
+		return (write(1, "0", 1));
+	else
+		ft_pthex(nb, format);
+	return (ft_hexlen(nb));
 }
