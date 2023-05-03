2b8b778e8b296f05fd55665c24c1e68f57025ff8
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-03 14:05:31 +0930

    Added changes to printf_un_int.c file

--- cfiles/printf_un_int.c
+++ cfiles/printf_un_int.c
@@ -6,8 +6,25 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 15:34:41 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/01 15:39:00 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/05/03 14:04:02 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
+
+static size_t	ft_un_int_len(unsigned int nb)
+{
+	size_t	len;
+
+	len = 0;
+	while (nb != 0)
+	{
+		len++;
+		nb /= 10;
+	}
+	return (len);
+}
+
+static char	*ft_unitoa(unsigned int n)
+{
+}
