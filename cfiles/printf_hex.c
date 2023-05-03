5f8625cd4038b23791506b8db4c5bd94f8d1f82a
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-03 14:51:06 +0930

    Added printf_hex.c file

---
+++ cfiles/printf_hex.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_hex.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 14:48:05 by btomlins          #+#    #+#             */
+/*   Updated: 2023/05/03 14:49:44 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../f_printf.h"
+
+static int	ft_hexlen(unsigned int nb)
+{
+}
+
+static void	ft_pthex(unsigned int nb, const char format)
+{
+}
+
+int	printf_pthex(unsigned int nb, const char format)
+{
+}
