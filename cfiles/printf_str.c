437d30f8998df6bbc1b939a6f21ddf29e0e8b6dc
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-04 12:47:07 +0930

    Modified printf_str.c file

--- cfiles/printf_str.c
+++ cfiles/printf_str.c
@@ -6,8 +6,32 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:56:25 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/03 16:57:19 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/05/04 12:44:19 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
+
+static void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		write(1, &str[i++], 1);
+}
+
+int	printf_ptstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+	{
+		ft_ptstr("(null)");
+		return (6);
+	}
+	while (str[i] != '\0')
+		write(1, &str[i++], 1);
+	return (i);
+}
