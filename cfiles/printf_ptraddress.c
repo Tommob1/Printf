85bf9bc59c427fb8b5bdf6ec10f30372f5986b10
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-04 13:22:46 +0930

    Added printf_ptraddress.c file

---
+++ cfiles/printf_ptraddress.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_ptraddress.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 12:50:11 by btomlins          #+#    #+#             */
+/*   Updated: 2023/05/04 13:21:18 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_prtinf.h"
+
+static int	ft_ptrlen(uniptr_t nb)
+{
+}
+
+static void	ft_ptptr(uniptr_t nb)
+{
+}
+
+int	printf_ptr(unsigned long long ptr)
+{
+}
