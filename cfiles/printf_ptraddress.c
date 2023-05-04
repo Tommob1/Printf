729eebc1f94f4195ad828935cc395a3ecfdb2207
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-04 13:46:29 +0930

    Modified printf_ptraddress.c file

--- cfiles/printf_ptraddress.c
+++ cfiles/printf_ptraddress.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 12:50:11 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/04 13:21:18 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/05/04 13:45:24 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,45 @@
 
 static int	ft_ptrlen(uniptr_t nb)
 {
+	int	len;
+
+	len = 0;
+	while (nb != 0)
+	{
+		len++;
+		nb /= 16;
+	}
+	return (len);
 }
 
 static void	ft_ptptr(uniptr_t nb)
 {
+	if (nb >= 16)
+	{
+		ft_ptptr(nb / 16);
+		ft_ptptr(nb % 16);
+	}
+	else
+	{
+		if (nb <= 9)
+			printf_char(nb + 48);
+		else
+			printf_char(nb - 10 + 97);
+	}
 }
 
 int	printf_ptr(unsigned long long ptr)
 {
+	int	print_len;
+
+	print_len = 0;
+	print_len += write(1, "0x", 2);
+	if (ptr == 0)
+		print_len += write(1, "0", 1);
+	else
+	{
+		ft_ptptr(ptr);
+		print_len += ft_ptrlen(ptr);
+	}
+	return (print_len);
 }
