b0a34e64171368188c549b52879629813f54c3ac
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-05 15:02:42 +0930

    Modified printf.c file

--- cfiles/printf.c
+++ cfiles/printf.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 10:06:36 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/04 17:48:32 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/05/05 15:01:38 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../ft_printf.h"
@@ -22,8 +22,18 @@ static int	format_eval(va_list args, const char format)
 		len += printf_pt_unint (va_arg(args, unsigned int));
 	else if (format == 'X' || format == 'x')
 		len += printf_pthex(va_arg(args, unsigned int), format);
+	else if (format == 'p')
+		len += printf_ptr(va_arg(args, unsigned long long));
+	else if (format == 's')
+		len += printf_ptstr(va_arg(args, char *));
+	else if (format == 'd' || format == 'i')
+		len += printf_ptint(va_arg(args, int));
+	else if (format == '%')
+		len += printf_pt_pcent();
+	return (len);
 }
 
 int	ft_printf(const char *format, ...)
 {
+
 }
