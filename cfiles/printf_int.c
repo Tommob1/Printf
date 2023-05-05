b1ae4eb06ce6fbb686adc9fb8d1f43f76cb656ac
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-05 16:33:08 +0930

    Added corrections to file

--- cfiles/printf_int.c
+++ cfiles/printf_int.c
@@ -6,13 +6,13 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 14:01:51 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/01 15:29:57 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:32:21 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-int	printf_int(int nb)
+int	printf_ptint(int nb)
 {
 	int		len;
 	char	*num;
